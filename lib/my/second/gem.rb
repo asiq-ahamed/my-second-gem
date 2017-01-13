ef90c64a525aa4f2dedf846f6a37514a927a954b
Asiq Ahamed <asiq@codoid.com>
2017-01-13 23:57:09 +0530

    Initial Upload

---
+++ lib/my/second/gem.rb
@@ -0,0 +1,9 @@
+require "my/second/gem/version"
+
+module My
+  module Second
+    module Gem
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/my/second/gem/version.rb
@@ -0,0 +1,7 @@
+module My
+  module Second
+    module Gem
+      VERSION = "0.1.0"
+    end
+  end
+end
